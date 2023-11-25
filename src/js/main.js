95855fca185081c1ab012c1fc3fc52e0d0ed0f73
weterynarzfred <weterynarzfred@gmail.com>
2023-11-25 13:21:11 +0100

    encoding slabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,58 @@
-import { decodeSlab, readSlab } from './talespire';
+import { decodeSlab, encodeSlab, readSlab, writeSlab } from './talespire';
 
-const exampleSlab = '```H4sIAAAAAAAACjv369xFJgZmBgYGgUWHGX9Pme/S4z7T7pZdoRojUCzUoTlb86ioz+KHr77rVr33BontTk44nxfD7rj55RyeqOPhGUxAsRQGMGDezvCEcQLbAaBxJxhgwIENRAIAg0JdKmgAAAA=```';
+const readSlabPaste = '```H4sIAAAAAAAACjv369xFJgZmBgYGgUWHGX9Pme/S4z7T7pZdoRojUCzUoTlb86ioz+KHr77rVr33BontTk44nxfD7rj55RyeqOPhGUxAsRQGMGDezvCEcQLbAaBxHOwObCChExAZBgD13dgLaAAAAA==```';
 
-const slabBinaryData = decodeSlab(exampleSlab);
-const slabData = readSlab(slabBinaryData);
-// console.log(slabData);
+const readSlabData = readSlab(decodeSlab(readSlabPaste));
 
-const formattedElement = document.createElement('pre');
-formattedElement.innerText = JSON.stringify(slabData, null, 2);
-document.getElementById('content').append(formattedElement);
+const writeSlabPaste = encodeSlab(writeSlab([
+  {
+    "uuid": "01c3a210-94fb-449f-8c47-993eda3e7126",
+    "assets": [
+      {
+        "x": 1,
+        "y": 0,
+        "z": 0,
+        "rotation": 180
+      }
+    ]
+  },
+  {
+    "uuid": "6b834055-c529-4c15-a3e1-eaf72d7aef4b",
+    "assets": [
+      {
+        "x": 1.83,
+        "y": 1.21,
+        "z": 1.05,
+        "rotation": 225
+      }
+    ]
+  },
+  {
+    "uuid": "cf6063bb-5c6e-4107-b3e9-9c0c5ac75768",
+    "assets": [
+      {
+        "x": 0,
+        "y": 0.5,
+        "z": 0,
+        "rotation": 0
+      },
+      {
+        "x": 0,
+        "y": 0,
+        "z": 10,
+        "rotation": 0
+      }
+    ]
+  }
+]));
+
+// const slabBinaryData = decodeSlab(testSlabPaste);
+// const slabData = readSlab(readSlabBinaryData);
+// console.log(readSlabData);
+
+document.getElementById('content').innerHTML = `
+<h2>read slab:</h2>
+<pre>${JSON.stringify(readSlabData, null, 2)}</pre>
+<h2>written slab:</h2>
+<pre>${writeSlabPaste}</pre>
+`;
--- src/js/talespire.js
+++ src/js/talespire.js
@@ -15,6 +15,8 @@ function parseIndex(assetIndex) {
   for (const key in assetIndex) {
     const assetArray = assetIndex[key];
     if (!Array.isArray(assetArray)) continue;
+    if (['Music'].includes(key)) continue;
+
     for (const asset of assetArray) {
       if (asset.Id === undefined) continue;
 
@@ -32,17 +34,17 @@ function parseIndex(assetIndex) {
       parsedIndex[asset.Id] = assetData;
     }
   }
-  console.log(parsedIndex);
 
   return parsedIndex;
 }
 
-function readSlab(binaryData) {
-  const buffer = binaryData.buffer;
+function readSlab(buffer) {
+  // console.log(Array.from(new Uint8Array(buffer)));
   let bufferPointer = 0;
 
   // header
-  new Uint32Array(buffer.slice(bufferPointer, bufferPointer += 4))[0];
+  const header = new Uint32Array(buffer.slice(bufferPointer, bufferPointer += 4))[0];
+  // console.log('header: ' + header);
 
   const version = new Uint16Array(buffer.slice(bufferPointer, bufferPointer += 2))[0];
   // console.log('version: ' + version);
@@ -105,37 +107,101 @@ function readSlab(binaryData) {
   return layouts;
 }
 
+const HEADER_SIZE = 10;
+const LAYOUT_SIZE = 20;
+const ASSET_SIZE = 8;
+const ADDITIONAL_DATA_SIZE = 2; // ?
+function writeSlab(layouts) {
+  const header = 0xD1CEFACE;
+  const version = 2;
+  const layoutCount = layouts.length;
+  const totalAssetCount = layouts.reduce((sum, layout) => sum + layout.assets.length, 0);
+
+  const buffer = new ArrayBuffer(
+    HEADER_SIZE +
+    layoutCount * LAYOUT_SIZE +
+    totalAssetCount * ASSET_SIZE +
+    ADDITIONAL_DATA_SIZE
+  );
+  const view = new DataView(buffer);
+  let bufferPointer = 0;
+
+  view.setUint32(bufferPointer, header, true);
+  bufferPointer += 4;
+
+  view.setUint16(bufferPointer, version, true);
+  bufferPointer += 2;
+
+  view.setUint16(bufferPointer, layoutCount, true);
+  bufferPointer += 2;
+
+  // skip creature count bytes (not in use)
+  bufferPointer += 2;
+
+  for (const layout of layouts) {
+    const uuidArray = layout.uuid.split('-');
+    view.setUint32(bufferPointer, parseInt(uuidArray[0], 16), true);
+    bufferPointer += 4;
+    view.setUint16(bufferPointer, parseInt(uuidArray[1], 16), true);
+    bufferPointer += 2;
+    view.setUint16(bufferPointer, parseInt(uuidArray[2], 16), true);
+    bufferPointer += 2;
+    view.setUint8(bufferPointer, parseInt(uuidArray[3].slice(0, 2), 16), true);
+    bufferPointer += 1;
+    view.setUint8(bufferPointer, parseInt(uuidArray[3].slice(2), 16), true);
+    bufferPointer += 1;
+    Array.from(uuidArray[4].matchAll(/[0-9a-f]{2}/g)).forEach(e => {
+      view.setUint8(bufferPointer, parseInt(e[0], 16), true);
+      bufferPointer += 1;
+    });
+    view.setUint16(bufferPointer, layout.assets.length, true);
+    bufferPointer += 2;
+
+    // skip _reserved_ bytes (?)
+    bufferPointer += 2;
+  }
+
+  for (const layout of layouts) {
+    for (const asset of layout.assets) {
+      const assetBinaryString = [
+        "00000",
+        Math.round(asset.rotation / 15).toString(2).padStart(5, '0'),
+        Math.round(asset.z * 100).toString(2).padStart(18, '0'),
+        Math.round(asset.y * 100).toString(2).padStart(18, '0'),
+        Math.round(asset.x * 100).toString(2).padStart(18, '0'),
+      ].join('');
+      Array.from(assetBinaryString.matchAll(/[0-1]{8}/g)).reverse().forEach(e => {
+        view.setUint8(bufferPointer, parseInt(e[0], 2), true);
+        bufferPointer += 1;
+      });
+    }
+  }
+
+  return buffer;
+}
+
 function decodeSlab(paste) {
   if (!paste) throw "slab empty";
 
-  paste = paste.replace(/[` ]/g, "");
+  const binaryString = atob(paste.replace(/[` ]/g, ""));
+  const bitArray = binaryString.split('').map(x => x.charCodeAt(0));
+  const deflatedData = new Uint8Array(bitArray);
+  const data = pako.inflate(deflatedData);
 
-  let binaryString;
-  try {
-    binaryString = atob(paste);
-  } catch {
-    throw "slab not in base64 format";
-  }
+  return data.buffer;
+}
 
-  let deflatedData;
-  try {
-    const bitArray = binaryString.split('').map(x => x.charCodeAt(0));
-    deflatedData = new Uint8Array(bitArray);
-  } catch {
-    throw "binary data malformed";
-  }
+function encodeSlab(binaryData) {
+  const bitData = pako.deflate(binaryData);
+  const binaryString = String.fromCharCode(...Array.from(bitData));
+  const paste = btoa(binaryString);
 
-  let data;
-  try {
-    data = pako.inflate(deflatedData);
-  } catch {
-    throw "unable to extract data";
-  }
-
-  return data;
+  return '```' + paste + '```';
 }
 
 export {
-  decodeSlab,
   readSlab,
+  writeSlab,
+  decodeSlab,
+  encodeSlab,
 };
