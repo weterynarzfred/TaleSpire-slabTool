618849943d50875e69e7b6fd175192d1255e3599
weterynarzfred <weterynarzfred@gmail.com>
2023-12-11 22:16:02 +0100

    multi axis scale

--- src/components/blocks/BlockScale.jsx
+++ src/components/blocks/BlockScale.jsx
@@ -2,21 +2,15 @@ import classNames from 'classnames';
 import BlockContents from '../blockParts/BlockContents';
 import BlockHeader from '../blockParts/BlockHeader';
 import BlockInput from '../blockParts/BlockInput';
-import BlockSelectInput from '../blockParts/BlockSelectInput';
 
 export default function BlockScale({ block }) {
-  const options = [
-    { value: 'x', label: 'X' },
-    { value: 'y', label: 'Y' },
-    { value: 'z', label: 'Z' },
-  ];
-
   return <div className={classNames(`block block--${block.type}`, { "block--is-collapsed": block.isCollapsed })}>
     <BlockHeader block={block} />
 
     <BlockContents block={block}>
-      <BlockSelectInput path={block.path} dataPath={['axis']} options={options} def={options[0]} />
-      <BlockInput path={block.path} dataPath={['scale']} def={1} />
+      <BlockInput path={block.path} dataPath={['scale', 'x']} def={1} />
+      <BlockInput path={block.path} dataPath={['scale', 'y']} def={1} />
+      <BlockInput path={block.path} dataPath={['scale', 'z']} def={1} />
     </BlockContents>
   </div>;
 };
--- src/lib/layoutUtils/scale.js
+++ src/lib/layoutUtils/scale.js
@@ -1,19 +1,37 @@
 import Layout from '../Layout';
 
-Layout.prototype.scale = function ({ axis = 'x', scale = 1 }) {
-  let minimum = Infinity;
-  let maximum = -Infinity;
+Layout.prototype.scale = function ({ scale = { x: 1, y: 1, z: 1 } }) {
+  scale.x = parseFloat(scale.x);
+  scale.x = isNaN(scale.x) ? 1 : scale.x;
+  scale.y = parseFloat(scale.y);
+  scale.y = isNaN(scale.y) ? 1 : scale.y;
+  scale.z = parseFloat(scale.z);
+  scale.z = isNaN(scale.z) ? 1 : scale.z;
+
+  let minimum = { x: Infinity, y: Infinity, z: Infinity };
+  let maximum = { x: -Infinity, y: -Infinity, z: -Infinity };
+
   for (const layout of this.layouts) {
     for (const asset of layout.assets) {
-      minimum = Math.min(minimum, asset[axis]);
-      maximum = Math.max(maximum, asset[axis]);
+      minimum.x = Math.min(minimum.x, asset['x']);
+      minimum.y = Math.min(minimum.y, asset['y']);
+      minimum.z = Math.min(minimum.z, asset['z']);
+      maximum.x = Math.max(maximum.x, asset['x']);
+      maximum.y = Math.max(maximum.y, asset['y']);
+      maximum.z = Math.max(maximum.z, asset['z']);
     }
   }
-  const center = (maximum + minimum) / 2;
+  const center = {
+    x: (maximum.x + minimum.x) / 2,
+    y: (maximum.y + minimum.y) / 2,
+    z: (maximum.z + minimum.z) / 2,
+  };
 
   for (let i = 0; i < this.layouts.length; i++) {
     for (let j = 0; j < this.layouts[i].assets.length; j++) {
-      this.layouts[i].assets[j][axis] = (this.layouts[i].assets[j][axis] - center) * scale + center;
+      this.layouts[i].assets[j].x = (this.layouts[i].assets[j].x - center.x) * scale.x + center.x;
+      this.layouts[i].assets[j].y = (this.layouts[i].assets[j].y - center.y) * scale.y + center.y;
+      this.layouts[i].assets[j].z = (this.layouts[i].assets[j].z - center.z) * scale.z + center.z;
     }
   }
 
