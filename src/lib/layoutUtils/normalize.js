bd81e06d0cb2a38de89e53b7e0ed316269fea77e
weterynarzfred <weterynarzfred@gmail.com>
2023-12-13 23:03:06 +0100

    clamp positions

--- src/lib/layoutUtils/normalize.js
+++ src/lib/layoutUtils/normalize.js
@@ -12,9 +12,9 @@ Layout.prototype.normalize = function () {
 
   for (const layout of this.layouts) {
     for (const asset of layout.assets) {
-      asset.x = Math.round((asset.x - minimums.x) * 1e6) / 1e6;
-      asset.y = Math.round((asset.y - minimums.y) * 1e6) / 1e6;
-      asset.z = Math.round((asset.z - minimums.z) * 1e6) / 1e6;
+      asset.x = Math.min(Math.round((asset.x - minimums.x) * 1e6) / 1e6, 2000);
+      asset.y = Math.min(Math.round((asset.y - minimums.y) * 1e6) / 1e6, 2000);
+      asset.z = Math.min(Math.round((asset.z - minimums.z) * 1e6) / 1e6, 2000);
       asset.rotation = Math.round(asset.rotation / 15) * 15;
       asset.rotation = (asset.rotation % 360 + 360) % 360;
     }
