fe35bb7d49f75ecdef538f59978689bf28466546
PsiaLapa <krzysciak.mateusz@gmail.com>
2024-02-23 23:38:52 +0000

    Added math to Duplicate Block

--- src/lib/layoutUtils/duplicate.js
+++ src/lib/layoutUtils/duplicate.js
@@ -1,19 +1,25 @@
 import Layout from '../Layout';
+import parseInput from '../parseInput';
 import { applyBlock } from '../reducer/recalculateLayout';
 
-Layout.prototype.duplicate = function ({ count = 1, modifiers = "relative" }, blocks = {}) {
+Layout.prototype.duplicate = function ({ count = 1, modifiers = "relative" }, blocks = {}, scope = {}) {
+
+  const usedCount = parseInput('integer', count, 1, scope);
   const initialLayout = this.clone();
   let lastLayout = this.clone();
-  for (let i = 1; i < count; i++) {
+  for (let i = 1; i < usedCount; i++) {
+    scope.iter = i;
     if (modifiers === 'relative') {
-      for (const id in blocks) applyBlock(lastLayout, blocks[id], { iter: i });
+      for (const id in blocks) applyBlock(lastLayout, blocks[id], scope);
       this.add(lastLayout.clone());
     } else if (modifiers === 'absolute') {
       lastLayout = initialLayout.clone();
-      for (const id in blocks) applyBlock(lastLayout, blocks[id], { iter: i });
+      for (const id in blocks) applyBlock(lastLayout, blocks[id], scope);
       this.add(lastLayout);
     }
   }
+
+  delete scope.iter;
 
   return this.cleanup();
 };
