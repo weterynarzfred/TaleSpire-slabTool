a38afcc160c9e4dad0ca3bc092ac47701eb26e28
weterynarzfred <weterynarzfred@gmail.com>
2025-03-15 14:33:28 +0100

    rudimentary template saving

--- src/components/TemplateSaving.jsx
+++ src/components/TemplateSaving.jsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react";
 import { useTrackedState, useUpdate } from './StateProvider';
 
 export default function TemplateSaving() {
+  const [templates, setTemplates] = useState([]);
   const state = useTrackedState();
   const dispatch = useUpdate();
 
@@ -11,8 +13,49 @@ export default function TemplateSaving() {
     });
   }
 
+  function handleTemplateSave() {
+    const newTemplates = [
+      ...templates,
+      {
+        name: new Date().getTime(),
+        blocks: state.blocks,
+      }
+    ];
+    setTemplates(newTemplates);
+    TS.localStorage.global.setBlob(JSON.stringify(newTemplates));
+  }
+
+  function handleTemplateDelete(event) {
+    const template = templates[event.currentTarget.closest('.template-item').dataset['index']];
+    const newTemplates = templates.filter(e => e.name !== template.name);
+    setTemplates(newTemplates);
+    TS.localStorage.global.setBlob(JSON.stringify(newTemplates));
+  }
+
+  function handleTemplateLoad(event) {
+    const template = templates[event.currentTarget.closest('.template-item').dataset['index']];
+    console.log(template);
+    dispatch({
+      type: 'REPLACE_BLOCKS',
+      value: JSON.stringify(template.blocks),
+    });
+  }
+
+  useEffect(() => {
+    (async () => {
+      console.log('reading local storage');
+      const savedTemplates = JSON.parse(await TS.localStorage.global.getBlob() || '[]');
+      console.log(savedTemplates);
+      setTemplates(savedTemplates);
+    })();
+  }, []);
+
   return <div className="TemplateSaving">
-    <p><strong>current template</strong> (temporary fix) – copy and paste only, don't edit by hand or you might crash the symbiote:</p>
+    <button onClick={handleTemplateSave}>save current template</button>
+    {templates.map((template, index) => <div className="template-item" key={index} data-index={index}>
+      <div onClick={handleTemplateLoad}>{template.name}</div>
+      <button onClick={handleTemplateDelete}>×</button>
+    </div>)}
     <textarea value={JSON.stringify(state.blocks)} onChange={handleInputChange} spellCheck={false} />
   </div>;
 }
