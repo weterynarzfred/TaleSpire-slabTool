e87155db6a7f4a093fd6681a68d79a89749ebfcb
weterynarzfred <weterynarzfred@gmail.com>
2025-03-16 22:17:07 +0100

    renaming templates

--- src/components/TemplateSaving.jsx
+++ src/components/TemplateSaving.jsx
@@ -3,6 +3,8 @@ import { useTrackedState, useUpdate } from './StateProvider';
 
 export default function TemplateSaving() {
   const [templates, setTemplates] = useState([]);
+  const [renaming, setRenaming] = useState(null);
+  const [templateName, setTemplateName] = useState("");
   const state = useTrackedState();
   const dispatch = useUpdate();
 
@@ -40,6 +42,23 @@ export default function TemplateSaving() {
     });
   }
 
+  function handleTemplateRenameButton(event) {
+    const index = event.currentTarget.closest('.template-item').dataset['index'];
+    setRenaming(index);
+    setTemplateName(templates[index].name);
+  }
+
+  function handleTemplateRenameSubmit(event) {
+    event.preventDefault();
+    const input = event.currentTarget.querySelector('input');
+    console.log(input.value);
+    const newTemplates = JSON.parse(JSON.stringify(templates));
+    newTemplates[renaming].name = input.value;
+    setTemplates(newTemplates);
+    TS.localStorage.global.setBlob(JSON.stringify(newTemplates));
+    setRenaming(null);
+  }
+
   useEffect(() => {
     (async () => {
       const savedTemplates = JSON.parse(await TS.localStorage.global.getBlob() || '[]');
@@ -49,9 +68,15 @@ export default function TemplateSaving() {
   }, []);
 
   return <div className="TemplateSaving">
+    {renaming === null ? null : <div className="TempalteSaving__rename">
+      <form onSubmit={handleTemplateRenameSubmit}>
+        <input type="text" value={templateName} onChange={event => { setTemplateName(event.currentTarget.value); }} />
+      </form>
+    </div>}
     <button onClick={handleTemplateSave}>save current template</button>
     {templates.map((template, index) => <div className="template-item" key={index} data-index={index}>
       <div onClick={handleTemplateLoad}>{template.name}</div>
+      <button onClick={handleTemplateRenameButton}>rename</button>
       <button onClick={handleTemplateDelete}>×</button>
     </div>)}
     <textarea value={JSON.stringify(state.blocks)} onChange={handleInputChange} spellCheck={false} />
