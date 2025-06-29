3b7a1f0a750088ad8c9514e983740cdf3d559768
PsiaLapa <krzysciak.mateusz@gmail.com>
2025-06-29 12:54:34 +0200

    computeTemplateResult fix (clone blocks)

--- src/lib/computeTemplateResult.js
+++ src/lib/computeTemplateResult.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import Layout from "../lib/Layout";
 import { evaluate } from "mathjs";
 import { applyBlock } from "../lib/reducer/recalculateLayout";
@@ -6,20 +7,19 @@ export async function computeTemplateResult({ blocks, templateHeader }) {
   const layout = new Layout();
   const scope = {};
 
-  // Evaluate header safely
+  const clonedBlocks = _.cloneDeep(blocks || {});
+
   try {
     evaluate(templateHeader, scope);
   } catch (e) {
     throw new Error("Invalid templateHeader: " + e.message);
   }
 
-  // Apply blocks
-  const blockArray = Object.values(blocks || {}).sort((a, b) => a.order - b.order);
+  const blockArray = Object.values(clonedBlocks).sort((a, b) => a.order - b.order);
   for (const block of blockArray) {
     applyBlock(layout, block, scope);
   }
 
   layout.normalize();
-
   return layout.base64;
 }
