fce3d6167b8de9ac9b8cf6b3552c58a954c7d256
PsiaLapa <krzysciak.mateusz@gmail.com>
2025-07-03 02:40:31 +0200

    Auto growing template header

--- src/components/TemplateHeader.jsx
+++ src/components/TemplateHeader.jsx
@@ -1,11 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useTrackedState, useUpdate } from "./StateProvider";
 
 let changeTimeout;
 export default function TemplateHeader() {
   const [inputValue, setInputValue] = useState("");
+  const textareaRef = useRef(null);
   const state = useTrackedState();
   const dispatch = useUpdate();
+
+  useEffect(() => {
+    const el = textareaRef.current;
+    if (el) {
+      el.style.height = "auto";
+      const minHeight = 10 * 10;
+      el.style.height = Math.max(el.scrollHeight+14, minHeight) + "px";
+    }
+  }, [inputValue]);
 
   useEffect(() => {
     clearTimeout(changeTimeout);
@@ -25,13 +35,17 @@ export default function TemplateHeader() {
     setInputValue(state.templateHeader);
   }, [state.stateReplacementIndex]);
 
-  return <div className="TemplateHeader default-tooltip-anchor" data-tooltip-key="header">
-    {state.templateHeaderError !== undefined ? <div className="block__error">{state.templateHeaderError}</div> : null}
-    <textarea
-      className="template-header-input"
-      value={inputValue}
-      placeholder={"# Comments\n\nGlobal variables:\nx = 5\nvariable = 7"}
-      onChange={(e) => setInputValue(e.target.value)}
-    />
-  </div>;
+  return (
+    <div className="TemplateHeader default-tooltip-anchor" data-tooltip-key="header">
+      {state.templateHeaderError !== undefined && (
+        <div className="block__error">{state.templateHeaderError}</div>
+      )}
+      <textarea
+        ref={textareaRef}
+        value={inputValue}
+        placeholder={"# Comments\n\nGlobal variables:\nx = 5\nvariable = 7"}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+    </div>
+  );
 }
