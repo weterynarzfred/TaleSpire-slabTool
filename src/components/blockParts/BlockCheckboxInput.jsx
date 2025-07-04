846ff263b5112847dbe91e9431c621723a162430
PsiaLapa <krzysciak.mateusz@gmail.com>
2025-07-04 20:50:14 +0200

    Added def to BlockCheckboxInput field

--- src/components/blockParts/BlockCheckboxInput.jsx
+++ src/components/blockParts/BlockCheckboxInput.jsx
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import { useTrackedState, useUpdate } from '../StateProvider';
 
-export default function BlockCheckboxInput({ dataPath, path, tooltip }) {
+export default function BlockCheckboxInput({ dataPath, path, tooltip, def = false }) {
   const state = useTrackedState();
   const dispatch = useUpdate();
 
@@ -15,17 +15,18 @@ export default function BlockCheckboxInput({ dataPath, path, tooltip }) {
   }
 
   const value = _.get(state.blocks, path.join('.blocks.') + '.data.' + dataPath.join('.'));
+  const checkedValue = value !== undefined ? value : def;
 
   return <div className='BlockInput BlockInputCheckbox'>
-    <label>
-      <span
-        className={tooltip ? "label default-tooltip-anchor" : "label"}
-        data-tooltip-key={tooltip}
-      >
-        {dataPath.join('.').replace('_', ' ')}:
-      </span>
-      <input type="checkbox" checked={value ?? false} onChange={handleCheckbox} />
-      <div className="checkbox-display default-tooltip-anchor" data-tooltip-key={tooltip}>{value ? '✓' : ''}</div>
-    </label>
-  </div>;
+      <label>
+        <span
+          className={tooltip ? "label default-tooltip-anchor" : "label"}
+          data-tooltip-key={tooltip}
+        >
+          {dataPath.join('.').replace('_', ' ')}:
+        </span>
+        <input type="checkbox" checked={checkedValue} onChange={handleCheckbox} />
+        <div className="checkbox-display default-tooltip-anchor" data-tooltip-key={tooltip}>{checkedValue ? '✓' : ''}</div>
+      </label>
+    </div>;
 }
